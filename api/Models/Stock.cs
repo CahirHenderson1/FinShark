2ff2e4069182bf81ab0e507d3cd6a766848df5d4
CahirHenderson1 <cahir.henderson@foodsconnected.com>
2025-04-29 10:30:21 +0100

    Exercise 1-4

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace api.Models
 {
-    [Table("Stocks")]
     public class Stock
     {
         public int Id { get; set; }
@@ -20,7 +19,5 @@ namespace api.Models
         public long MarketCap { get; set; }
 
         public List<Comment> Comments { get; set; } = new List<Comment>();
-        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
-
     }
 }
